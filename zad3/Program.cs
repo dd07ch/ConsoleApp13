5b865249d1048fc7e680e4c59b9a4b03289e9db9
MEGA <MEGA@gmail.com>
2025-02-14 17:15:00 +0200

    zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете число:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (IsPowerOfTwo(n))
+            {
+                Console.WriteLine($"{n} е степен на 2.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} не е степен на 2.");
+            }
+        }
+
+        static bool IsPowerOfTwo(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+
+            if (n <= 0 || n % 2 != 0)
+            {
+                return false;
+            }
+
+            return IsPowerOfTwo(n / 2);
+        }
+    }
+}
+  
